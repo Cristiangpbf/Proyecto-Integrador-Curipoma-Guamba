2d50dbf3a935b55b18f5223a7ed6488004791667
Cristiangpbf <cristiangpbf@gmail.com>
2021-07-06 18:24:45 -0500

    Creacion de Migraciones bdd_name: cono_superior

---
+++ Laravel/api/database/migrations/2021_07_06_204511_create_productions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->double('total sales');
+            $table->double('liters');
+            $table->string('time');
+            $table->string('performance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productions');
+    }
+}
---
+++ Laravel/api/database/migrations/2021_07_06_204528_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('dimensions');
+            $table->string('flavor');
+            $table->string('texture');
+            $table->string('consumption_time');
+            $table->string('img_url');
+            $table->text('description');
+            $table->string('package_amount');/*Unidades que hay en el paquete*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
