462a777de5538cbe04685834ed39eba16d085760
Cristiangpbf <cristiangpbf@gmail.com>
2021-08-01 17:13:46 -0500

    Añadir categorias a recursos products

--- Laravel/api/app/Http/Controllers/ProductController.php
+++ Laravel/api/app/Http/Controllers/ProductController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Category;
 use App\Product;
 use App\Http\Resources\Product as ProductResource;
+use App\Http\Resources\ProductCollection;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(){
-        return Product::all();
+        return response()->json(new ProductCollection(Product::all()), 200);
     }
 
     public function indexFiltered(Category $category){
